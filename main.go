e12103629f795b19cd9040e801f0f9b7f59e2e90
code-tigger <1785260184@qq.com>
2024-06-17 22:39:32 +0800

    2024.6,17

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
+import "althmetic/test1"
 
+func main() {
+	nums := []int{100, 4, 200, 1, 3, 2}
+	test1.LongestConsecutive(nums)
 }
--- test1/2024.6.17.go
+++ test1/2024.6.17.go
@@ -1,23 +1,34 @@
-package main
+package test1
+
+import "fmt"
 
 //128. 最长连续序列
 //给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
 //请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
 
-func longestConsecutive(nums []int) int {
+func LongestConsecutive(nums []int) int {
+	fmt.Printf("nums:%v", nums)
+	fmt.Println()
 	numset := map[int]bool{}
 	for _, num := range nums {
 		numset[num] = true
 	}
+	fmt.Printf("numset:%v", numset)
+	fmt.Println()
 	longestStreak := 0
 	for num := range numset {
+		fmt.Println(num)
 		if !numset[num-1] {
 			currentNum := num
 			currentStreak := 1
 			for numset[currentNum+1] {
 				currentNum++
-
+				currentStreak++
+			}
+			if longestStreak < currentStreak {
+				longestStreak = currentStreak
 			}
 		}
 	}
+	return longestStreak
 }
